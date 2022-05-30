f956fc1871695284c455cb2395ca80d268c3469b
jepul <jepul@jepul>
2022-05-30 09:03:58 +0900

    Kolb 설무조사 개발

--- src/main/java/com/fas/model/repository/SvyResponseInfoRepository.java
+++ src/main/java/com/fas/model/repository/SvyResponseInfoRepository.java
@@ -1,5 +1,17 @@
 package com.fas.model.repository;
 
-public interface SvyResponseInfoRepository {
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.fas.model.svy.domain.SvyReponseInfoDomain;
+
+public interface SvyResponseInfoRepository  extends JpaRepository<SvyReponseInfoDomain, String>{
+
+	@Query("select u from SvyReponseInfoDomain u where u.userId = ?1 and u.svyId = ?2 order by u.svyRepUnqNo desc")
+	public List<SvyReponseInfoDomain> findByUserIdAndSvyIdOrderBySvyRepUnqNoDesc(String userId, String svyId);
+	
+	@Query("select u from SvyReponseInfoDomain u where u.svyRepUnqNo = ?1 and u.userId = ?2 order by u.svyItmId desc")
+	public List<SvyReponseInfoDomain> findBySvyRepUnqNoAndUserIdOrderBySvyItmIdDesc(String svyRepUnqNo, String userId);
 }
--- src/main/java/com/fas/model/repository/SvyResultInfoRepository.java
+++ src/main/java/com/fas/model/repository/SvyResultInfoRepository.java
@@ -1,5 +1,18 @@
 package com.fas.model.repository;
 
-public interface SvyResultInfoRepository {
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.fas.model.svy.domain.SvyResultInfoDomain;
+
+public interface SvyResultInfoRepository  extends JpaRepository<SvyResultInfoDomain, String>{
+
+	@Query("select u from SvyResultInfoDomain u where u.userId = ?1 and u.svyId = ?2 order by u.seq desc")
+	public List<SvyResultInfoDomain> findByUserIdAndSvyIdOrderBySeqDesc(String userId, String svyId);
+	
+	@Query("select u from SvyResultInfoDomain u where u.svyId = ?1 and u.userId = ?2 and u.lastYn = 'Y' order by u.seq desc")
+	public List<SvyResultInfoDomain> findBySvyIdAndUserIdOrderBySeq(String svyId, String userId);
+	
 }
