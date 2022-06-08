62536ea6ea0ee8a6a8e7dfcda8b4e1d749cfdf98
jepul <jepul@jepul>
2022-06-08 17:04:00 +0900

    테스트

--- src/main/java/com/fas/model/repository/SvyResultInfoRepository.java
+++ src/main/java/com/fas/model/repository/SvyResultInfoRepository.java
@@ -9,10 +9,12 @@ import com.fas.model.svy.domain.SvyResultInfoDomain;
 
 public interface SvyResultInfoRepository  extends JpaRepository<SvyResultInfoDomain, String> {
 
+	/****
 	@Query("select u from SvyResultInfoDomain u where u.userId = ?1 and u.svyId = ?2 order by u.seq desc")
 	public List<SvyResultInfoDomain> findByUserIdAndSvyIdOrderBySeqDesc(String userId, String svyId);
 	
 	@Query("select u from SvyResultInfoDomain u where u.svyId = ?1 and u.userId = ?2 and u.lastYn = 'Y' order by u.seq desc")
 	public List<SvyResultInfoDomain> findBySvyIdAndUserIdOrderBySeq(String svyId, String userId);
+    *******/
 	
 }
