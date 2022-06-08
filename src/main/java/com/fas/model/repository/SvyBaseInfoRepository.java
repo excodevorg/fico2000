797c78ec24b254a3c60dd8787cb5ed58c9fa1200
jepul <jepul@jepul>
2022-06-08 17:14:50 +0900

    test

--- src/main/java/com/fas/model/repository/SvyBaseInfoRepository.java
+++ src/main/java/com/fas/model/repository/SvyBaseInfoRepository.java
@@ -7,7 +7,10 @@ import com.fas.model.svy.domain.SvyBaseInfoDomain;
 
 public interface SvyBaseInfoRepository extends JpaRepository<SvyBaseInfoDomain, String> {
 
+	/***
 	@Query("select u from SvyBaseInfoDomain u where u.useYn = 'Y' and u.svyId = ?1")
 	public SvyBaseInfoDomain findBySvyId(String svyId);
+	****/
+	
 	
 }
